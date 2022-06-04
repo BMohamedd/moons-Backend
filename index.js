09d9a63ecb941ce95a9dc9c60d0485c0f8d38b1a
BMohamedd <89010936+BMohamedd@users.noreply.github.com>
2022-06-04 11:23:41 +0200

    added a wild card "*" to the cors origin

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.json({ limit: "2mb" }));
 app.use(express.urlencoded({ extended: true, limit: "2mb" }));
-app.use(cors());
+app.use(cors({ origin: "*" }));
 
 //routes
 app.use(require("./routes/Routes"));
